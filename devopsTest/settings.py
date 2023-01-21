9339ab798c6f10c1a7d543cd288f972e47232e5c
3v1lkrot666 <doctorstone20@gmail.com>
2023-01-21 03:22:08 +0300

    deploy start version project

--- devopsTest/settings.py
+++ devopsTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ns)@es=qeq)^@3mqm5em5gie3e-4m9isx+@@5t7i3hf^59we*@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
